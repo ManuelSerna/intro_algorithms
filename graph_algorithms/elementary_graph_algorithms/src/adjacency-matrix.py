039fb95a7e2ec26e6210d1c737d0b333a21443ae
Manuel Serna-Aguilera <manuelserna1997@gmail.com>
2019-07-21 21:42:44 -0500

    Added list and matrix implementations for representing a graph.

---
+++ graph_algorithms/elementary_graph_algorithms/src/adjacency-matrix.py
@@ -0,0 +1,28 @@
+#************************************************
+# Adjacency-matrix representation of a graph.
+#************************************************
+
+class AdjacencyMatrix():
+    # Create |V| x |V| (n x n) matrix adj
+    def __init__(self, n):
+        self.n = n
+        self.adj = []
+        for i in range(self.n):
+            row = []
+            for j in range(n):
+                row.append(0)
+            self.adj.append(row)
+
+    def print_adj(self):
+        for i in range(self.n):
+            print(self.adj[i])
+
+    # Connect vertices u and v to make edge (u, v) with weight w (1 default)
+    def insert(self, u, v, w=1):
+        self.adj[u][v] = w
+        print('Inserted edge ({}, {}).'.format(u, v))
+
+    # Remove edge (u, v)
+    def remove(self, u, v):
+        self.adj[u][v] = 0
+        print('Removed edge ({}, {}).'.format(u, v))
